0ae3e5cd9d79e8e61de9be9b96f45afd42462260
judinus10 <judes.20222403@iit.ac.lk>
2025-02-08 18:55:48 +0530

    load the images

--- src/PacMan.java
+++ src/PacMan.java
@@ -15,7 +15,7 @@ public class PacMan extends JPanel {
     private Image blueGhostImage;
     private Image orangeGhostImage;
     private Image pinkGhostImage;
-    private Image redGhostimage;
+    private Image redGhostImage;
 
     private Image pacmanUp;
     private Image pacmanDown;
@@ -25,5 +25,18 @@ public class PacMan extends JPanel {
     PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
+
+        //load images
+        wallImage = new ImageIcon(getClass().getResource("assets/wall.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("assets/blueGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("assets/orangeGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("assets/redGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("assets/pinkGhost.png")).getImage();
+
+        pacmanUp = new ImageIcon(getClass().getResource("assets/pacmanUp.png")).getImage();
+        pacmanDown = new ImageIcon(getClass().getResource("assets/pacmanDown.png")).getImage();
+        pacmanLeft = new ImageIcon(getClass().getResource("assets/pacmanLeft.png")).getImage();
+        pacmanRight = new ImageIcon(getClass().getResource("assets/pacmanRight.png")).getImage();
+
     }
 }
