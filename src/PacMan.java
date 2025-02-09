976a2687e758cef24c20b34f7619bc7ddb882284
judinus10 <judes.20222403@iit.ac.lk>
2025-02-09 15:27:25 +0530

    Hashset created for storing unique elements and fast lookup operations

--- src/PacMan.java
+++ src/PacMan.java
@@ -44,6 +44,11 @@ public class PacMan extends JPanel {
     private Image pacmanRight;
     private Image pacmanLeft;
 
+    HashSet<Block> walls;
+    HashSet<Block> foods;
+    HashSet<Block> ghosts;
+    Block pacman;
+
     PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
