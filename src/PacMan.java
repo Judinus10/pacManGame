f6a147997c0d75c015da6238a974fdc8aeab074c
judinus10 <judes.20222403@iit.ac.lk>
2025-02-10 15:48:50 +0530

    move assets folder to src

--- src/PacMan.java
+++ src/PacMan.java
@@ -80,16 +80,16 @@ public class PacMan extends JPanel {
         setBackground(Color.BLACK);
 
         //load images
-        wallImage = new ImageIcon(getClass().getResource("assets/wall.png")).getImage();
-        blueGhostImage = new ImageIcon(getClass().getResource("assets/blueGhost.png")).getImage();
-        orangeGhostImage = new ImageIcon(getClass().getResource("assets/orangeGhost.png")).getImage();
-        redGhostImage = new ImageIcon(getClass().getResource("assets/redGhost.png")).getImage();
-        pinkGhostImage = new ImageIcon(getClass().getResource("assets/pinkGhost.png")).getImage();
+        wallImage = new ImageIcon(getClass().getResource("./assets/wall.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("./assets/blueGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("./assets/orangeGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("./assets/redGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("./assets/pinkGhost.png")).getImage();
 
-        pacmanUp = new ImageIcon(getClass().getResource("assets/pacmanUp.png")).getImage();
-        pacmanDown = new ImageIcon(getClass().getResource("assets/pacmanDown.png")).getImage();
-        pacmanLeft = new ImageIcon(getClass().getResource("assets/pacmanLeft.png")).getImage();
-        pacmanRight = new ImageIcon(getClass().getResource("assets/pacmanRight.png")).getImage();
+        pacmanUp = new ImageIcon(getClass().getResource("./assets/pacmanUp.png")).getImage();
+        pacmanDown = new ImageIcon(getClass().getResource("./assets/pacmanDown.png")).getImage();
+        pacmanLeft = new ImageIcon(getClass().getResource("./assets/pacmanLeft.png")).getImage();
+        pacmanRight = new ImageIcon(getClass().getResource("./assets/pacmanRight.png")).getImage();
 
     }
 
